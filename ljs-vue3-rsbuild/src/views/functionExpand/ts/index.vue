97f2594586ff4315507303499f6a6241cce7ecff
姚龙飞 <38839364@qq.com>
2025-04-29 15:07:56 +0800

    功能拓展

---
+++ ljs-vue3-rsbuild/src/views/functionExpand/ts/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="comPage">
+    ts版：喜欢ts。或需要组合式可用的API时，使用该版本。后期会增加ts版的增删查改模板，敬请期待。
+  </div>
+</template>
+
+<script setup lang="ts">
+  onMounted(() => {
+    initData();
+  });
+  async function initData() {
+    console.log('ts版');
+  }
+  defineExpose({
+    initData
+  })
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
